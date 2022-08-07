accaa3872847c30cea9a4915ccb66506c36a96d6
samssi <samir.hamadi@gmail.com>
2022-08-07 20:01:12 +0300

    Refactor: DRY

--- src/webgl-renderer/webgl.ts
+++ src/webgl-renderer/webgl.ts
@@ -3,11 +3,15 @@ export interface Config {
     height: number;
 }
 
-export const createShader = (gl: WebGL2RenderingContext, type: GLenum, source: string) => {
-    const shader = gl.createShader(type);
-    if (shader === null) {
-        throw new Error("Failed to create a WebGL shader!")
+export const webGLEntityOrError = <T> (webGlEntity: T | null): T => {
+    if (webGlEntity === null) {
+        throw new Error("Failed to create WebGL entity!");
     }
+    return webGlEntity;
+}
+
+export const createShader = (gl: WebGL2RenderingContext, type: GLenum, source: string) => {
+    const shader = webGLEntityOrError(gl.createShader(type));
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
@@ -20,10 +24,7 @@ export const createShader = (gl: WebGL2RenderingContext, type: GLenum, source: s
 }
 
 export const createProgram = (gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) => {
-    const program = gl.createProgram();
-    if (program === null) {
-        throw new Error("Failed to create a WebGL program!")
-    }
+    const program = webGLEntityOrError(gl.createProgram());
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
