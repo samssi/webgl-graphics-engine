32925e64eeac6494b06e6f08987ef3047ff636b9
samssi <samir.hamadi@gmail.com>
2022-08-25 00:09:06 +0300

    Move the triangle only

--- src/input/keyboard.ts
+++ src/input/keyboard.ts
@@ -27,25 +27,33 @@ const keyPress = (event: KeyboardEvent) => {
     if (functionality === "left") {
         const newTri = applicationState.triangles()[0];
         newTri[0].x = newTri[0].x - 10;
+        newTri[1].x = newTri[1].x - 10;
+        newTri[2].x = newTri[2].x - 10;
         applicationState.setTriangles([newTri]);
     }
     if (functionality === "right") {
         const newTri = applicationState.triangles()[0];
         newTri[0].x = newTri[0].x + 10;
+        newTri[1].x = newTri[1].x + 10;
+        newTri[2].x = newTri[2].x + 10;
         applicationState.setTriangles([newTri]);
     }
 
     if (functionality === "up") {
         const newTri = applicationState.triangles()[0];
-        newTri[1].x = newTri[1].x - 10;
-        applicationState.setTriangles([newTri]);
-    }
-    if (functionality === "down") {
-        const newTri = applicationState.triangles()[0];
-        newTri[1].x = newTri[1].x + 10;
+        newTri[0].y = newTri[0].y + 10;
+        newTri[1].y = newTri[1].y + 10;
+        newTri[2].y = newTri[2].y + 10;
         applicationState.setTriangles([newTri]);
     }
 
+    if (functionality === "down") {
+        const newTri = applicationState.triangles()[0];
+        newTri[0].y = newTri[0].y - 10;
+        newTri[1].y = newTri[1].y - 10;
+        newTri[2].y = newTri[2].y - 10;
+        applicationState.setTriangles([newTri]);
+    }
 }
 
 export const keyboardListener = () => {
